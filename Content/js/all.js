ca1da4d358c617b553283947f53e92c450661ab5
wwchen <wwchen412@gmail.com>
2017-12-05 16:07:41 +0800

    faq done

--- Content/js/all.js
+++ Content/js/all.js
@@ -1,2 +1,10 @@
 // header.js
 $('header').load('index.html header');
+
+// faq.js
+$('.faq-q').each(function() {
+    $(this).click(function() {
+
+        $(this).siblings('.faq-a').slideToggle();
+    })
+})
